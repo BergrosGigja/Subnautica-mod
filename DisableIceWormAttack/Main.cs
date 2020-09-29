d8ea035b4a3ddc78be19180fc716c4d35d467978
Bergrós Gígja <bergros.gigja@gmail.com>
2020-09-29 23:30:12 +0000

    add option to disable or not in menu

--- DisableIceWormAttack/Main.cs
+++ DisableIceWormAttack/Main.cs
@@ -1,5 +1,9 @@
 ﻿using HarmonyLib;
 using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Options;
+using SMLHelper.V2.Options.Attributes;
 using System.Reflection;
 using Logger = QModManager.Utility.Logger;
 
@@ -8,11 +12,25 @@ namespace DisableIceWormAttack
     [QModCore]
     public static class Main
     {
+        internal static Config config { get; } = OptionsPanelHandler.RegisterModOptions<Config>();
+
         [QModPatch]
         public static void Patch()
         {
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.disablewormattack.psmod");
             Logger.Log(Logger.Level.Info, "Disable Worm Attack Mod Patched!");
+        }
+    }
+
+    [Menu("Options Menu")]
+    public class Config : ConfigFile
+    {
+        [Toggle("Ice Worm Attack Disabled"), OnChange(nameof(MyCheckboxToggleEvent))]
+        public bool ToggleValue = false;
+
+        private void MyCheckboxToggleEvent(ToggleChangedEventArgs e)
+        {
+            Logger.Log(Logger.Level.Info, $"Ice Worm Attack Disabled: {e.Value}");
         }
     }
 
@@ -22,7 +40,7 @@ namespace DisableIceWormAttack
         [HarmonyPrefix]
         internal static bool Prefix()
         {
-            return false;
+            return !(Main.config.ToggleValue);
         }
     }
 }
