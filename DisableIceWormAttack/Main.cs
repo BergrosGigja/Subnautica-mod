136e54c0d5ed5a9669df69c5e26a11bb679ee144
Bergrós Gígja <bergros.gigja@gmail.com>
2020-09-29 23:17:39 +0000

    add prefix to disable Iceworm attacks on player and json

---
+++ DisableIceWormAttack/Main.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+using Logger = QModManager.Utility.Logger;
+
+namespace DisableIceWormAttack
+{
+    [QModCore]
+    public static class Main
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "com.disablewormattack.psmod");
+            Logger.Log(Logger.Level.Info, "Disable Worm Attack Mod Patched!");
+        }
+    }
+
+    [HarmonyPatch(typeof(IceWormHuntModeTrigger), nameof(IceWormHuntModeTrigger.OnPlayerEnter))]
+    internal static class IceWormHuntModeTrigger_Override
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
